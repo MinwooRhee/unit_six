d35f263c0d6b89f07d575b8e4b3eb4f69b42f2bd
Min Woo <minwoo@iMac5.local>
2018-10-18 14:30:04 -0400

    using list indices

---
+++ d1_unit_six_warmups.py
@@ -0,0 +1,12 @@
+
+students = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+
+print(students[0])
+
+print(students[2])
+
+print(students[-1])
+
+students.insert(5, "George")
+
+print(students)
