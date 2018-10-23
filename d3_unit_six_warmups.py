ab2a98447ea4730af2b770903832e9ed20b477bf
Min Woo <minwoo@iMac5.local>
2018-10-23 09:12:10 -0400

    modifying a list

---
+++ d3_unit_six_warmups.py
@@ -0,0 +1,12 @@
+
+students = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+
+students.remove("Fred")
+
+del students[4]
+
+students[1] = "Bill"
+
+students.append("Parvati")
+
+print(students)
